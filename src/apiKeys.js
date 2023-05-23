7864870e8d45d1215bd20c586af73f409f355ccc
Alchemy2901 <ajaydesai550@gmail.com>
2023-05-23 22:09:49 +0530

    ..

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f8e6fa398f45ac5ec3ba95fa3bad02a7",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
